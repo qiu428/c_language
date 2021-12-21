b9647a37c05f6b9b477c2e0e55459b151d739000
qiu1248 <2453424080@qq.com>
2021-12-21 16:24:45 +0800

    判断字符是否唯一

---
+++ LeetCode/q2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isUnique(char *astr)
+{
+    int ascii[128] = {0};
+    for (int i = 0; i < strlen(astr); i++)
+    {
+        if (*(ascii + astr[i]) == 1)
+            return false;
+        else
+            *(ascii + astr[i]) += 1;
+    }
+    return true;
+}
+int main(void)
+{
+    char s[] = "leetcode";
+    printf("%s\n", isUnique(s) ? "true" : "false");
+    return 0;
+}
