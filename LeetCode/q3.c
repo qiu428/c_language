344cdef8609342228c62b476d48df8d9f797a74d
qiu1248 <2453424080@qq.com>
2021-12-23 14:26:34 +0800

    leetcode

---
+++ LeetCode/q3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <limits.h>
+int myAtoi(char *s);
+
+int main()
+{
+    printf("%ld", myAtoi("  -eswqe"));
+    return 0;
+}
+
+int myAtoi(char *s)
+{
+    int digit = 0;
+    int PN = 1;
+    while (*s == ' ')
+        s++;
+    if (*s++ == '-')
+        PN = -1;
+    while (*s >= '0' && *s <= '9')
+    {
+        int num = *s - '0';
+        if (digit < INT_MAX / 10 || (digit == INT_MAX / 10 && num < 8))
+        {
+            digit = digit * 10 + num;
+        }
+        else
+            return PN == 1 ? INT_MAX : INT_MIN;
+        s++;
+    }
+    return digit * PN;
+}
